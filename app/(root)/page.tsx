44345c041ef232c22fb138a5d77204dbbdf40e91
mykola-umrikhin <umrihin2611@gmail.com>
2026-02-11 11:18:43 +0200

    Implement Local Search

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,89 @@
-const Home = async () => {
+import LocalSearch from "@/components/search/LocalSearch";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+import Link from "next/link";
+
+const questions = [
+  {
+    _id: "1",
+    title: "How to learn React?",
+    description:
+      "I am new to web development and I want to learn React. Can anyone suggest some good resources?",
+    tags: [
+      {
+        _id: "1",
+        name: "react",
+      },
+      {
+        _id: "2",
+        name: "javascript",
+      },
+    ],
+    author: { _id: "1", name: "John Doe" },
+    upvotes: 10,
+    answers: 5,
+    views: 100,
+    createdAt: new Date(),
+  },
+  {
+    _id: "2",
+    title: "What is the difference between var, let and const in JavaScript?",
+    description:
+      "I am confused about the differences between var, let and const in JavaScript. Can someone explain it to me?",
+    tags: [
+      {
+        _id: "2",
+        name: "javascript",
+      },
+    ],
+    author: { _id: "2", name: "Jane Smith" },
+    upvotes: 5,
+    answers: 3,
+    views: 50,
+    createdAt: new Date(),
+  },
+];
+interface SearchParams {
+  searchParams: Promise<{ [key: string]: string }>;
+}
+
+const Home = async ({ searchParams }: SearchParams) => {
+  const { query } = await searchParams;
+
+  const filteredQuestions = questions.filter((question) =>
+    question.title.toLowerCase().includes(query?.toLowerCase() || ""),
+  );
+
   return (
-    <div className="flex h-svh items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className="">Hello Next</h1>
-    </div>
+    <>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
+        <Button
+          className="primary-gradient !text-light-900 min-h-[46px] px-4 py-3"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+
+      <section className="mt-10">
+        <LocalSearch
+          route="/"
+          imgSrc="/icons/search.svg"
+          placeholder="Search questions..."
+          otherClasses="flex-1"
+        />
+      </section>
+
+      <p>Home Filter</p>
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {filteredQuestions.map((question) => (
+          <h1 key={question._id}>{question.title}</h1>
+        ))}
+      </div>
+    </>
   );
 };
 
---
+++ components/search/LocalSearch.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import { Input } from "../ui/input";
+import { formUrlQuery, removeKeysUrlQuery } from "@/lib/url";
+
+interface Props {
+  route: string;
+  imgSrc: string;
+  placeholder: string;
+  otherClasses?: string;
+}
+
+const LocalSearch = ({ route, imgSrc, placeholder, otherClasses }: Props) => {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const searchParams = useSearchParams();
+  const query = searchParams.get("query") || "";
+
+  const [searchQuery, setSearchQuery] = useState(query);
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (searchQuery) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "query",
+          value: searchQuery,
+        });
+        router.push(newUrl, { scroll: false });
+      } else {
+        if (pathname === route) {
+          const newUrl = removeKeysUrlQuery({
+            params: searchParams.toString(),
+            keysToRemove: ["query"],
+          });
+          router.push(newUrl, { scroll: false });
+        }
+      }
+    }, 300);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [searchQuery, router, pathname, route]);
+
+  return (
+    <div
+      className={`background-light800_darkgradient flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}
+    >
+      <Image
+        src={imgSrc}
+        width={24}
+        height={24}
+        alt="search icon"
+        className="cursor-pointer"
+      />
+      <Input
+        type="text"
+        placeholder={placeholder}
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        className="paragraph-regular no-focus placeholder text-dark400_light700 border-none shadow-none outline-none"
+      />
+    </div>
+  );
+};
+
+export default LocalSearch;
